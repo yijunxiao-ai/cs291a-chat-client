ca72c218f1bbaacc9d5b0bd5cd7cd243a523157c
Yijun Xiao <yijunxiao@cs.ucsb.edu>
2019-10-28 20:46:37 -0700

    first commit

---
+++ precache-manifest.89729553b59f047bb0fa55ec816d7ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "585e5b1327187d10aa562cd9b6a5223d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa22338686fa1252024b",
+    "url": "/static/css/main.0ba470bb.chunk.css"
+  },
+  {
+    "revision": "f8db4db8813f8ac13f0e",
+    "url": "/static/js/2.17c36bce.chunk.js"
+  },
+  {
+    "revision": "fa22338686fa1252024b",
+    "url": "/static/js/main.77e43a7d.chunk.js"
+  },
+  {
+    "revision": "be45126ea0614fb87c08",
+    "url": "/static/js/runtime-main.2d721f2a.js"
+  }
+]);
